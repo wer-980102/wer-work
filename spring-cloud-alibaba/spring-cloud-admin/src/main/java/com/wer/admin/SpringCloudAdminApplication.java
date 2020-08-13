e3b901ee81a3c50e993f532f8eb430bbcbae61f2
wer <1578535923@qq.com>
2020-08-13 16:12:12 +0800

    编写完admin监控

--- spring-cloud-alibaba/spring-cloud-admin/src/main/java/com/wer/admin/SpringCloudAdminApplication.java
+++ spring-cloud-alibaba/spring-cloud-admin/src/main/java/com/wer/admin/SpringCloudAdminApplication.java
@@ -1,8 +1,10 @@
 package com.wer.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer //代码手动开启
 @SpringBootApplication
 public class SpringCloudAdminApplication {
 
